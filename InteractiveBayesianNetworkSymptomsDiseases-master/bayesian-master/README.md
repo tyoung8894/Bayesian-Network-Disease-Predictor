72a7dbbb62a520cc537795baabe5bc9dd48900ce
tyoung8894 <tyoung12@elon.edu>
2017-12-14 14:52:03 -0500

    updated classes names/readme/comments

--- InteractiveBayesianNetworkSymptomsDiseases-master/bayesian-master/README.md
+++ InteractiveBayesianNetworkSymptomsDiseases-master/bayesian-master/README.md
@@ -1,2 +1,59 @@
-bayesian
-========
+Author: Tyler Young
+
+**Bayesian network for symptoms and diseases**
+
+This program takes data from a PubMed database dataset of symptoms and diseases
+from https://www.nature.com/articles/ncomms5212
+
+The dataset used is taken from: https://images.nature.com/original/nature-assets/ncomms/2014/140626/ncomms5212/extref/ncomms5212-s4.txt
+
+This is the data.txt file in the project.
+
+
+The dataset lists all the relationships of symptoms/diseases found in published articles in the 
+PubMed database, with a TDIF score of how frequent the symptom was associated with the disease.
+There are over 140,000 symptom/disease relationships.
+
+
+I created a table in MySQL of the first 44,000 entries and used these as my dataset for generating the 
+bayesian network.  The database has three columns: symptom_name, disease_name, and score
+
+This program used the Weka API and JavaBayes API 
+WEKA: http://weka.sourceforge.net/doc.dev/overview-summary.html
+JavaBayes: http://www.cs.cmu.edu/~javabayes/
+http://www.cs.cmu.edu/~javabayes/EBayes/index.html/JavaDoc/index.html
+
+The JavaBayes source has not been updated for a long time and is made in Java 1, but I found a project that 
+allowed JavaBayes to compile with higher versions of Java : https://dataworks-inc.com/simple-bayesian-network-inference-using-netica-and-javabayes/
+I used https://github.com/joeschweitzer/javabayes as my project and used my own code so it would compile
+
+
+
+How it works:
+
+dataset.txt --> MySQL database --> formatted text file using CsvConverter.java----> save generated text file with .arff extension --> use .arff file as input to BayesianNetworkGenerator.java --> used generated .xml file as input to UserGUI.java --> run UserGUI2.java
+
+The CSVConverter.java creates .txt file from the MySQL database of symptoms and diseases that 
+then has to manually be saved by the user as an .arff file so it can be loaded into the WEKA API to 
+generate the bayesian network.  Info on .arff files here: https://www.cs.waikato.ac.nz/ml/weka/arff.html
+Make sure last line of .arff file is complete, the writer sometimes cuts off at the end. If it isn't delete that line
+Also make sure the NAME tag in the .arff file doesn't contain any characters other than letters or it won't work
+
+The .arff file that is saved should be used as the input DataSource file in BayesianNetworGenerator.java.
+This class generates a BIF XML of the network which can then be imported into the JavaBayes API.  This
+BIF XML should be set to the InferenceGraph file input at the beginning of UserGUI.java.  
+Make sure the <NAME> tag of the BIF XML file only contains letters, if it doesn't change the name or it won't work
+
+
+I had to use two APIs since Weka does not have functionality for inference, but is great for generating
+the bayesian networks and learning the probability distributions.  JavaBayes is great for 
+running inference using posterior probabilities of events given evidence, but is not good for generating
+the networks since you have to build everything manually.  
+
+The main class to run the entire program is in UserGUI2.java, so run this class to run the program.
+
+
+For example of what BIF XML should look like, open formatted.xml with a text editor
+For example of what .arff file should look like, open 125.arff with text editor
+Use networkBIF.xml as input in UserGUI to run the program with my generated file
+  
---
+++ InteractiveBayesianNetworkSymptomsDiseases-master/bayesian-master/src/tyleryoung/UserGUI2.java
@@ -0,0 +1,218 @@
+package tyleryoung;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+
+import javax.swing.*;
+import javax.swing.event.*;
+
+import javabayes.InferenceGraphs.InferenceGraph;
+import javabayes.InferenceGraphs.InferenceGraphNode;
+import javabayes.InterchangeFormat.IFException;
+
+import java.util.*;
+
+
+/**
+ * @author Tyler Young
+ *
+ *main class of the project that runs the GUI
+ *read the README for info how this project works
+ *
+ *used https://docs.oracle.com/javase/tutorial/displayCode.html?code=https://docs.oracle.com/javase/tutorial/uiswing/examples/components/SplitPaneDemo2Project/src/components/SplitPaneDemo2.java
+ *as resource for general layout for my GUI
+ */
+public class UserGUI2 extends JFrame implements ListSelectionListener {
+	private JLabel label;
+	private JButton btnNewButton_1;
+	private JButton btnNewButton_2;
+	private JPanel panel;
+	private JSplitPane splitPane_1;
+	private JButton btnNewButton;
+	private JButton generateButton;
+	private JButton clearButton;
+	public UserGUI diseaseSymptomGUI;
+	public String symptomToBeAdded;
+	public ArrayList<String> observationSymptoms;
+
+	public UserGUI2() throws IOException, IFException {
+		super("Symptom Checker- Identify possible diseases for your symptoms");
+		//create a list that will hold the user's inputted symptoms
+		observationSymptoms = new ArrayList<String>();
+
+		//Create an instance of the GUI
+		UserGUI newGui = new UserGUI();
+		diseaseSymptomGUI = newGui;
+		JSplitPane top = diseaseSymptomGUI.getSplitPane();
+
+		//add selection listener to the list in gui
+		diseaseSymptomGUI.getImageList().addListSelectionListener(this);
+		top.setBorder(null);
+
+		//Provide minimum sizes for the two components in the split pane
+		top.setMinimumSize(new Dimension(100, 50));
+
+		//Clear button that removes all symptoms set to be observed
+		clearButton = new JButton("Clear");
+		clearButton.addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent e){
+				observationSymptoms.clear();
+				label.setText("Symptoms cleared.  Enter new set of symptoms");
+				try {
+					diseaseSymptomGUI.updateLabel("No symptoms added yet");
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				} catch (IFException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+
+		getContentPane().add(clearButton, BorderLayout.SOUTH);
+		panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.NORTH);
+		panel.setLayout(new BorderLayout(0, 0));
+		label = new JLabel("Click on a Symptom name in the list.",
+				SwingConstants.CENTER);
+
+		JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT,
+				top, label);
+		panel.add(splitPane, BorderLayout.NORTH);
+		splitPane.setOneTouchExpandable(true);
+		splitPane.setDividerLocation(180);
+
+		splitPane_1 = new JSplitPane();
+		panel.add(splitPane_1, BorderLayout.SOUTH);
+
+		//adds a button for selecting a symptom, if a symptom in the scroll list is 
+		//selected and the user clicks the button, the symptom will be added to the 
+		//list of symptoms to be set as observed
+		btnNewButton = new JButton("Add The Selected Symptom ");
+		btnNewButton.addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent e){
+				label.setText(symptomToBeAdded + " added to symptoms");
+				observationSymptoms.add(symptomToBeAdded);
+				String textToDisplay = "<html><p>Symptoms entered:</p> ";
+				int count = 0;
+				for(String userEnteredSymptom: observationSymptoms){
+					count++;
+					textToDisplay = textToDisplay+ "<p>" + count + ". " + userEnteredSymptom + "</p>";
+				}
+				textToDisplay = textToDisplay + "</html>";
+				try {
+					diseaseSymptomGUI.updateLabel(textToDisplay);
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				} catch (IFException e1) {
+					e1.printStackTrace();
+				}
+
+			}
+		});
+
+
+
+		splitPane_1.setLeftComponent(btnNewButton);
+		generateButton = new JButton("Generate Likely Diseases");
+
+		//if "generate likely diseases" button is clicked in gui, all of the symptoms the user
+		//has added will be set as observed variables in the graph, and the top diseases will output on the right label of the GUI
+		generateButton.addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent e){
+				UserGUI gui;
+				try {
+					gui = new UserGUI();
+					InferenceGraph graph = gui.generateGraph();
+					Vector<InferenceGraphNode> guiNodes = graph.get_nodes();
+					JavaBayes newBayes = new JavaBayes(graph, guiNodes);
+					//finds the top diseases and updates the label with the text of the disease names
+					//formats the label with html
+					ArrayList<String> likelyDiseases = newBayes.getBelief(observationSymptoms, guiNodes, graph);
+					try {
+						String diseaseOutcomeText = "<html><p>Top Diseases:</p> ";
+						int count=0;
+						for(String disease: likelyDiseases){
+							count++;
+							diseaseOutcomeText = diseaseOutcomeText + "<p>" + count + "." + disease + "</p>";
+						}
+						diseaseSymptomGUI.updateLabel(diseaseOutcomeText);
+					} catch (IOException e2) {
+						e2.printStackTrace();
+					} catch (IFException e2) {
+						e2.printStackTrace();
+					}
+
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				} catch (IFException e1) {
+					e1.printStackTrace();
+				}
+				//System.out.println("DISEASE OUTCOME: " + likelyDisease);
+			}
+		});
+		splitPane_1.setRightComponent(generateButton);
+		label.setMinimumSize(new Dimension(30, 30));
+	}
+
+
+
+	public JLabel getLabel(){
+		return label;
+	}
+
+
+
+	/* 
+	 * Changes the text of the label bar to the selected symptom name in the scroll panel of symptoms
+	 */
+	public void valueChanged(ListSelectionEvent e) {
+		if (e.getValueIsAdjusting())
+			return;
+
+		JList theList = (JList)e.getSource();
+		if (theList.isSelectionEmpty()) {
+			label.setText("Nothing selected.");
+		} else {
+			String symptomSelected = theList.getSelectedValue().toString();
+			int index = theList.getSelectedIndex();
+			symptomToBeAdded = symptomSelected;
+			label.setText("Selected Symptom:  " + symptomSelected);
+		}
+	}
+
+
+
+	private static void createAndShowGUI() throws IOException, IFException {
+		//Create and set up the window.
+		JFrame frame = new UserGUI2();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+
+
+	/**
+	 * @param args
+	 * main class to run the entire project
+	 */
+	public static void main(String[] args) {
+		//Schedule a job for the event-dispatching thread:
+		//creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					createAndShowGUI();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (IFException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
+
+
